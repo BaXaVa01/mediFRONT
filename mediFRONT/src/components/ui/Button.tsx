0fabbcb6edba65fe148ae0ae8908c5f1ead09913
baxava <barkalovalexander01@gmail.com>
2026-04-30 00:38:59 -0600

    refactor: extract cn utility to shared file

--- mediFRONT/src/components/ui/Button.tsx
+++ mediFRONT/src/components/ui/Button.tsx
@@ -1,11 +1,6 @@
 import { type VariantProps, cva } from 'class-variance-authority';
 import { type ButtonHTMLAttributes, forwardRef } from 'react';
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+import { cn } from '../../utils/cn';
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
--- mediFRONT/src/components/ui/Input.tsx
+++ mediFRONT/src/components/ui/Input.tsx
@@ -1,10 +1,5 @@
 import { forwardRef, type InputHTMLAttributes } from 'react';
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+import { cn } from '../../utils/cn';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
