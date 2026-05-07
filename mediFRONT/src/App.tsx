de06378a551602d78e180b5110d8ca52a25f8f03
baxava <barkalovalexander01@gmail.com>
2026-05-07 00:29:22 -0600

    feat(routing): add specific registration routes

--- mediFRONT/src/App.tsx
+++ mediFRONT/src/App.tsx
@@ -5,6 +5,9 @@ import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import RegisterPatientPage from './pages/RegisterPatientPage';
+import RegisterDoctorPage from './pages/RegisterDoctorPage';
+import RegisterClinicPage from './pages/RegisterClinicPage';
 import ProfilePage from './pages/ProfilePage';
 
 const App: React.FC = () => {
@@ -18,6 +21,9 @@ const App: React.FC = () => {
             <Route path="/buscar" element={<SearchPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/registro" element={<RegisterPage />} />
+            <Route path="/registro/paciente" element={<RegisterPatientPage />} />
+            <Route path="/registro/medico" element={<RegisterDoctorPage />} />
+            <Route path="/registro/clinica" element={<RegisterClinicPage />} />
             <Route path="/perfil" element={<ProfilePage />} />
             {/* Fallback to home */}
             <Route path="*" element={<HomePage />} />
